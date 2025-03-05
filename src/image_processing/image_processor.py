188d49a8deacd71a438abdb759cb715dbc0ec5ad
Javier Manotas Ruiz <jmanotasruiz@gmail.com>
2025-03-05 18:27:30 +0100

    feat: implement basic image to grid recognition

---
+++ src/image_processing/image_processor.py
@@ -0,0 +1,54 @@
+from PIL import Image
+from grid import Grid
+
+def dst(color0, color1):
+    return sum((color0[i] - color1[i]) ** 2 for i in range(3)) ** 0.5
+
+# colors picked from ./gnome_mines_template
+colors = {
+    0: [222,222,220],
+    1: [221,250,195],
+    2: [236,237,191],
+    3: [237,218,180],
+    4: [237,195,138],
+    5: [247,161,162],
+    6: [254,167,133],
+    7: [255,125,96],
+    8: [255,50,60],
+    '?': [186,189,182]
+}
+
+min_dst = min(dst(v, v1)
+              for (k, v) in colors.items()
+              for (k1, v1) in colors.items()
+              if k != k1)
+
+max_col_error = min_dst / 3
+
+def get_game_cell(image, min_row, min_col, max_row, max_col):
+
+    freqs = { k : 0 for k in colors.keys() }
+
+    for y in range(min_row, max_row):
+        for x in range(min_col, max_col):
+            color = image.getpixel((x, y))
+
+            for k, v in colors.items():
+                if dst(color, v) < min_dst:
+                    freqs[k] += 1
+                    break
+
+    return max(freqs, key=freqs.get)
+
+def image_to_grid(image_path, rows, cols):
+    image = Image.open(image_path, "r").convert('RGB')
+    width, _ = image.size
+    cell_size = width // rows
+
+    grid = Grid(rows, cols)
+
+    for r in range(rows):
+        for c in range(cols):
+            grid[(r, c)] = get_game_cell(image, cell_size * r, cell_size * c, cell_size * (r + 1), cell_size * (c + 1))
+
+    return grid
