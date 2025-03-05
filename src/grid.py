a8dfa78627706ef9710b6de1550ae54e6ac233ca
Javier Manotas Ruiz <jmanotasruiz@gmail.com>
2025-03-05 19:47:12 +0100

    fix: remove unnecesary type annotations from grid

--- src/grid.py
+++ src/grid.py
@@ -1,18 +1,16 @@
-from typing import *
-
 class Grid:
-    def __init__(self, rows: int, cols: int):
+    def __init__(self, rows, cols):
         self.rows = rows
         self.cols = cols
         self.data = [['?'] * cols for _ in range(rows)]
 
-    def __str__(self) -> str:
+    def __str__(self):
         return '\n'.join(''.join(str(cell) for cell in row) for row in self.data)
 
-    def __getitem__(self, index: Tuple[int, int]) -> int | str:
+    def __getitem__(self, index):
         row, col = index
         return self.data[row][col]
     
-    def __setitem__(self, index: Tuple[int, int], value: int | str):
+    def __setitem__(self, index, value):
         row, col = index
         self.data[row][col] = value
