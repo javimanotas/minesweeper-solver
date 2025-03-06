af6be7256abe78297644a271c5bd95458dabb6e6
Javier Manotas Ruiz <jmanotasruiz@gmail.com>
2025-03-06 18:19:11 +0100

    perf: improve solving time
    unknown cells with non numeric neighbors will no longer be considered

--- src/solving/solver.py
+++ src/solving/solver.py
@@ -1,13 +1,25 @@
 import clingo
 from grid import Grid
 
+def has_numeric_neighbor(grid, r, c):
+    for i in range(r - 1, r + 2):
+        for j in range(c - 1, c + 2):
+            if i < 0 or j < 0 or i >= grid.rows or j >= grid.cols:
+                continue
+
+            if isinstance(grid[(i, j)], int):
+                return True
+
+    return False
+
 def solve(grid):
 
     predicates = []
     for r in range(grid.rows):
         for c in range(grid.cols):
             if grid[(r, c)] == '?':
-                predicates.append(f'unknown({r}, {c}).')
+                if has_numeric_neighbor(grid, r, c):
+                    predicates.append(f'unknown({r}, {c}).')
             else:
                 predicates.append(f'at({r}, {c}, {grid[(r, c)]}).')
 
