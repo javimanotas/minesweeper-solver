b79d54d0241564198450b7daba2e4b4450d4098f
Javier Manotas Ruiz <jmanotasruiz@gmail.com>
2025-03-08 17:54:57 +0100

    feat: add debug mode

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import time
 from PIL import ImageGrab
@@ -6,23 +7,41 @@ from image_processing.image_processor import image_to_grid
 from solving import solver
 from display.display import display_solution
 
-image_path = './temp_screenshot.png'
+def show_usage():
+    print('Usage: python3 <path_to_main> [-debug]')
+    sys.exit(1)
 
-for i in range(2):
-    print(f'taking a screenshot in {3 - i}...')
-    time.sleep(1)
+def main():
+    debug = False
 
-screenshot = ImageGrab.grab()
-screenshot.save(image_path)
+    if len(sys.argv) > 2:
+        show_usage()
+    elif len(sys.argv) == 2:
+        if sys.argv[1] == '-debug':
+            debug = True
+        else:
+            show_usage()
 
-contour, rows, cols = find_contour(image_path)
-print(f'detected contour: {contour}')
+    image_path = './temp_screenshot.png'
 
-grid = image_to_grid(image_path, contour, rows, cols)
-print(grid)
+    for i in range(2):
+        print(f'taking a screenshot in {3 - i}...')
+        time.sleep(1)
 
-solution = solver.solve(grid)
-print(solution)
-display_solution(image_path, contour, grid, solution)
+    screenshot = ImageGrab.grab()
+    screenshot.save(image_path)
 
-os.remove(image_path)
+    contour, rows, cols = find_contour(image_path)
+    grid = image_to_grid(image_path, contour, rows, cols)
+    solution = solver.solve(grid)
+
+    if debug:
+        print(f'\ndetected contour: {contour}')
+        print(f'\ngrid:\n{grid}')
+        print(f'\n{solution}')
+
+    display_solution(image_path, contour, grid, solution)
+    os.remove(image_path)
+
+if __name__ == '__main__':
+    main()
