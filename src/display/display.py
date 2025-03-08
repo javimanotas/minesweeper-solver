f10ab6b88e62b257faf29a072fbbe8e1ff5d715e
Javier Manotas Ruiz <jmanotasruiz@gmail.com>
2025-03-08 16:17:35 +0100

    fix: adapt display to show only the game

--- src/display/display.py
+++ src/display/display.py
@@ -18,19 +18,19 @@ def center_at(screen, row, col, cell_size, res):
 
     return center_x - width / 2, center_y - height / 2
 
-def display_solution(image_path, grid, solution):
+def display_solution(image_path, contour, grid, solution):
     mines = solution['mine']
     safes = solution['safe']
 
     pygame.init()
     image = load_image(image_path)
-    width, height = image.get_size()
-    screen = pygame.display.set_mode((width, height))
+    x0, y0, x1, y1 = contour
+    screen = pygame.display.set_mode((x1 - x0, y1 - y0))
     pygame.display.set_caption('Solution')
 
-    screen.blit(image, (0, 0))
+    screen.blit(image, (-x0, -y0))
 
-    cell_size = width / grid.cols
+    cell_size = (x1 - x0) / grid.cols
 
     mine = load_image('./src/display/mine.png', cell_size * 0.8)
     not_mine = load_image('./src/display/not_mine.png', cell_size * 0.66)
